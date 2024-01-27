ae3bf5e14b74e0d849f5d70b1602391ede33573f
tinkuo6o71999 <20bcs099@nith.ac.in>
2024-01-27 13:09:14 +0530

    Project one is done

---
+++ 07projects/project_01_color_swithcer/script.js
@@ -0,0 +1,34 @@
+// simply select all the button and then select the body beaucse we have to change the color of the body
+
+const button = document.querySelectorAll('.button');
+ 
+//now we cal select the parent of each button seperatley like below
+button.forEach(function (item) {
+    const parent = item.parentElement;
+    // console.log(parent)
+    const newColor = item.textContent
+    // console.log(newColor)
+    
+})
+
+// another method bcz we know we have to slect the body bcs we have to chabge the color of body so we select it direclty
+
+const body = document.querySelector("body")
+// console.log(body)
+
+
+button.forEach(function (button) {
+    // console.log(button)
+    
+
+    // now we have to add event on each button
+    // there are many types of event like click, hover etc. here we handling the click event bcz we have to change the color of body on click 
+    button.addEventListener('click',function(e){
+      
+    //    console.log(e);
+    //   console.log(e.target)
+      const newColor = e.target.id;
+    //   console.log(newColor)
+      body.style.backgroundColor = newColor
+    })
+})
